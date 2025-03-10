c593c844c6a49d78e2ac8993b404e7e9fc7f284e
Deverver <jackRMrasmussen@gmail.com>
2025-03-10 09:44:34 +0100

    translated and played around

--- src/main/java/org/example/SimpleThread.java
+++ src/main/java/org/example/SimpleThread.java
@@ -3,10 +3,10 @@ package org.example;
 public class SimpleThread implements Runnable {
     @Override
     public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Kører tråd: " + Thread.currentThread().getName() + " - " + i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Running thread: " + Thread.currentThread().getName() + " - " + i);
             try {
-                Thread.sleep(1000); // Pauser tråden i 1 sekund
+                Thread.sleep(2000); // Pauses thread for 2 seconds
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -14,8 +14,11 @@ public class SimpleThread implements Runnable {
     }
 
     public static void main(String[] args) {
-        Thread thread = new Thread(new SimpleThread());
-        thread.start();
-        System.out.println("Main-metoden kører videre...");
-    }
-}
+        // Creates 5 threads running
+        for (int i = 1; i <= 5; i++) {
+            Thread thread = new Thread(new SimpleThread());
+            thread.start();
+        }
+        System.out.println("Main methods keeps on running...");
+    }// main END
+}// SimpleThread END
